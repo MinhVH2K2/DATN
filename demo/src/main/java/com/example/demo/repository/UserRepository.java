b0961ccb649860018bff09350e68f1c2e9ab369f
minh <minhvuhoang2k2@gmail.com>
2024-10-04 22:59:02 +0700

    update

---
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+   Optional<User>  findByuserName(String userName);
+
+   boolean existsByuserName(String userName);
+}
