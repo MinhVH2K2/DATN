0a69d255aa8344d86d32e54920e06f47513222a0
TrungSadest <145642838+TrungSadest@users.noreply.github.com>
2024-10-05 22:43:52 +0700

    up

---
+++ FE/src/app/layout/LeftMenu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/images/logo-dark.png";
+
+export default function LeftMenu() {
+  return (
+    <>
+      <div className="container">
+        <h1>Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click me</button>
+      </div>
+    </>
+  );
+}
---
+++ FE/src/app/router/indexRouter.tsx
@@ -0,0 +1,10 @@
+import Layout from "../layout/Layout";
+import Dashboard from "../page/dashboard/Dashboard";
+
+export const indexRouter: any = {
+  path: "/",
+  element: <Layout />,
+  children: [
+    { path: '/dashboard', element: <Dashboard /> },
+  ],
+};
