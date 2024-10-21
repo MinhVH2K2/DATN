5a8a5522daa09212314a00bfe3cbd45600a5ac11
namtv95it <145642838+TrungSadest@users.noreply.github.com>
2024-10-21 23:43:15 +0700

    category and product

--- FE/src/app/page/sales/CounterSale.tsx
+++ FE/src/app/page/sales/CounterSale.tsx
@@ -148,19 +148,116 @@ export default function CounterSale() {
             {/* Category */}
             <div
               style={{ width: "10%" }}
-              className="h-100 px-2 py-3 shadow-2 border-right"
+              className="h-100 px-2 py-3 shadow-2 border-end"
             >
-              <div className="cs-card shadow-1 active">
-                  Danh muc 1
+              <div className="cs-card shadow-1 active">Danh muc 1</div>
+              <div className="cs-card shadow-1">Danh muc 2</div>
+            </div>
+            <div
+              style={{ width: "60%", backgroundColor: "#f1f3f5" }}
+              className="h-100"
+            >
+              {/* Category name */}
+              <div className="px-3 pt-3">
+                <div className="d-flex justify-content-between w-100 bg-white rounded-3 px-4 py-3 border fw-semibold overflow-x-auto shadow-sm">
+                  <div className="d-flex">
+                    <div className="cs-order active">
+                      <span className="me-2">Hoa don 1</span>
+                      <i className="fa fa-xmark"></i>
+                    </div>
+                    <div className="cs-order">
+                      <span className="me-2">Hoa don 2</span>
+                      <i className="fa fa-xmark"></i>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <a className="pointer">
+                      <i className="fa fa-plus-circle fs-2"></i>
+                    </a>
+                  </div>
+                </div>
               </div>
-              <div className="cs-card shadow-1">
-                  Danh muc 2
+              {/* Products in category */}
+              <div className="d-flex h-100 w-100">
+                <div
+                  style={{ width: "25%", height: "120px" }}
+                  className="d-flex border rounded-3 bg-white m-3 shadow-sm p-2"
+                >
+                  <img
+                    style={{ width: "30%" }}
+                    src="https://product.hstatic.net/200000182297/product/3090418p1499dt_al621021932303010470p399dt_z103321512314910201p699dt_2__48b20874535c43eb804bc2f87f51a9e7_master.jpg"
+                    alt=""
+                  />
+                  <div className="d-flex flex-column justify-content-between align-items-center ms-2 fw-semibold">
+                    <p style={{ fontSize: "18px" }} className="m-0">
+                      Ao da nau
+                    </p>
+                    <div className="d-flex">
+                      <p className="m-0 text-danger">1.000.000đ</p>
+                      <del style={{fontSize: '12px'}} className="ms-1 mt-0 mb-0 me-0 text-secondary">1.000.000đ</del>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{ width: "25%", height: "120px" }}
+                  className="d-flex border rounded-3 bg-white m-3 shadow-sm p-2"
+                >
+                  <img
+                    style={{ width: "30%" }}
+                    src="https://product.hstatic.net/200000182297/product/10_e5d080b216404e03b73eb068db0cb014_master.jpg"
+                    alt=""
+                  />
+                  <div className="d-flex flex-column justify-content-between align-items-center ms-2 fw-semibold">
+                    <p style={{ fontSize: "18px" }} className="m-0">
+                      Ao da nau
+                    </p>
+                    <div className="d-flex">
+                      <p className="m-0 text-danger">1.000.000đ</p>
+                      <del style={{fontSize: '12px'}} className="ms-1 mt-0 mb-0 me-0 text-secondary">1.000.000đ</del>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{ width: "25%", height: "120px" }}
+                  className="d-flex border rounded-3 bg-white m-3 shadow-sm p-2"
+                >
+                  <img
+                    style={{ width: "30%" }}
+                    src="https://product.hstatic.net/200000182297/product/4_9b9b62ea47b54830a0af05cef65ba88b_master.jpg"
+                    alt=""
+                  />
+                  <div className="d-flex flex-column justify-content-between align-items-center ms-2 fw-semibold">
+                    <p style={{ fontSize: "18px" }} className="m-0">
+                      Ao da nau
+                    </p>
+                    <div className="d-flex">
+                      <p className="m-0 text-danger">1.000.000đ</p>
+                      <del style={{fontSize: '12px'}} className="ms-1 mt-0 mb-0 me-0 text-secondary">1.000.000đ</del>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  style={{ width: "25%", height: "120px" }}
+                  className="d-flex border rounded-3 bg-white m-3 shadow-sm p-2"
+                >
+                  <img
+                    style={{ width: "30%" }}
+                    src="https://product.hstatic.net/200000182297/product/3_13a51c6c04d04f818f70f02e784db963_master.jpg"
+                    alt=""
+                  />
+                  <div className="d-flex flex-column justify-content-between align-items-center ms-2 fw-semibold">
+                    <p style={{ fontSize: "18px" }} className="m-0">
+                      Ao da nau
+                    </p>
+                    <div className="d-flex">
+                      <p className="m-0 text-danger">1.000.000đ</p>
+                      <del style={{fontSize: '12px'}} className="ms-1 mt-0 mb-0 me-0 text-secondary">1.000.000đ</del>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div style={{ width: "60%" }} className="h-100">
-              2
-            </div>
-            <div style={{ width: "30%" }} className="h-100 shadow-1">
+            <div style={{ width: "30%" }} className="h-100 border-start">
               3
             </div>
           </div>
